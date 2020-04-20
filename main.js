37449815ada05de24edbf69bdbdd184bd7593cf0
sh0rtyB <brent@1mindsolutions.com>
2020-04-20 01:12:34 -0600

    refractoring functions

--- main.js
+++ main.js
@@ -3,51 +3,43 @@ const copyBtn = document.getElementById('copy')
 const html = document.getElementsByTagName("html")[0]
 const style1 = '--color'
 const style2 = '--color2'
+const styles = [style1, style2]
 let bgColor
 let color
 
-// todo working on
+// todo refactoring find/set colors
 // ?do I really need a one function solution?
+
 function findCssVal(ele, ...keys) {
-  const docStyles = document.documentElement.style
-  let winStyles = window.getComputedStyle(ele)
+  const winStyles = window.getComputedStyle(ele)
+  const keyValues = []
   for( const key of keys) {
-    const keyValue = winStyles.getPropertyValue(key)
-    docStyles.setProperty(key, keyValue)
-    console.log(key, keyValue)
+    let keyValue = winStyles.getPropertyValue(key)
+    keyValues.push(keyValue)
+    //console.log(keyValues);
   }
-} 
-
-findCssVal(html, style1, style2)
-
-// this works but maybe needs refractor 
-function findColors(ele, ...keys) {
-  bgColor = window.getComputedStyle(ele).getPropertyValue(keys[0])
-  color = window.getComputedStyle(ele).getPropertyValue(keys[1])
+  setColors(styles, ...keyValues)
 }
 
-function setColors(key, val, key2, val2) {
+//this works but maybe needs refractor 
+function setColors(keys, ...keyValues) {
   const docStyles = document.documentElement.style
-  docStyles.setProperty(key, val)
-  docStyles.setProperty(key2, val2)
+  docStyles.setProperty(keys[0], keyValues[0])
+  docStyles.setProperty(keys[1], keyValues[1])
 }
-
-// findColors(html, style1, style2)
-// setColors(style1, bgColor, style2, color)
 
 // event delegation
 pallet.addEventListener('click', e => {
   let btn = e.target.closest('button')
   // if (!btn) { return } //? is this better to use?
-  if (btn) { 
-    //findCssVal(btn, 'background-color', 'color')
-    findColors(btn, 'background-color', 'color')
-    setColors(style1, bgColor, style2, color)
+  if (btn) {   
+    findCssVal(btn, 'background-color', 'color')
     // drink the cool aid to see Random Colors!
     if (btn.classList[0] === 'random') {
       let random = Math.floor(Math.random()*16777215).toString(16)
       let randomBg = Math.floor(Math.random()*16777215).toString(16)
-      setColors('--randomBg', `#${randomBg}`, '--random', `#${random}`)
+      let randomStyle = ['--randomBg', '--random']
+      setColors(randomStyle, `#${randomBg}`, `#${random}`)
     } 
   } 
 })
